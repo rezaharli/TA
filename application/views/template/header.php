625de8405f15b49a56661a74196ed0b52a443a5d
Reza Harli <reza.harli@gmail.com>
2016-04-26 21:56:57 +0700

    fix include css dan js

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -17,18 +17,6 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/dist/css/skins/_all-skins.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/iCheck/flat/blue.css">
-    <!-- Morris chart -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/morris/morris.css">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/datepicker/datepicker3.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/daterangepicker/daterangepicker-bs3.css">
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,16 +27,9 @@
 
     <!-- jQuery 2.1.4 -->
     <script src="<?php echo base_url() ?>assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <!-- Bootstrap 3.3.5 -->
     <script src="<?php echo base_url() ?>assets/adminlte/bootstrap/js/bootstrap.min.js"></script>
 
-    
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-      $.widget.bridge('uibutton', $.ui.button);
-    </script>
   </head>
   <body class="hold-transition skin-blue sidebar-mini fixed">
     <div class="wrapper">
