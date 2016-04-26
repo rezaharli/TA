7a6fc70322369f0bc160c715d4420a4269048995
Reza Harli <reza.harli@gmail.com>
2016-04-27 01:09:42 +0700

    add google calendar ke halaman home

--- application/views/page/private/home.php
+++ application/views/page/private/home.php
@@ -1,7 +1,51 @@
-<!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-      <pre>
-        <?php var_dump($this->session->userdata()); ?>
-        </pre>
-      </div>
-      <!-- /.content-wrapper -->
+<!-- fullCalendar 2.7.0-->
+<link href='<?php echo base_url() ?>assets/fullcalendar-2.7.0/fullcalendar.css' rel='stylesheet' />
+<link href='<?php echo base_url() ?>assets/fullcalendar-2.7.0/fullcalendar.print.css' rel='stylesheet' media='print' />
+<!--Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+<section class="content">
+  <div class="row">
+    <div class="col-md-8">
+    </div>
+    <div class="col-md-4">
+      <div class="box box-primary">
+        <div class="box-body no-padding">
+          <!-- THE CALENDAR -->
+          <div id="calendar"></div>
+        </div><!-- /.box-body -->
+      </div><!-- /. box -->
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+</section><!-- /.content -->
+
+<pre>
+  <?php var_dump($this->session->userdata()); ?>
+  </pre>
+</div>
+<!-- /.content-wrapper -->
+
+<script src='<?php echo base_url() ?>assets/fullcalendar-2.7.0/lib/moment.min.js'></script>
+<script src='<?php echo base_url() ?>assets/fullcalendar-2.7.0/lib/jquery.min.js'></script>
+<script src='<?php echo base_url() ?>assets/fullcalendar-2.7.0/fullcalendar.min.js'></script>
+<script type='text/javascript' src='<?php echo base_url() ?>assets/fullcalendar-2.7.0/gcal.js'></script>
+<script>
+
+	$(document).ready(function() {
+
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'today',
+				center: 'title',
+				right: 'prev,next'
+			},
+			height: 'auto',
+			editable: true,
+			eventLimit: 2, // allow "more" link when too many events
+			events: '<?php echo base_url('home/calendar'); ?>'
+		});
+		
+	});
+
+</script>
+    
