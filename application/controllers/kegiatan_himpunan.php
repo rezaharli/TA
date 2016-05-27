445a459c3b2df20522dfdbb00ee5674d8d116e85
Anisatun Nafi'ah <blue.dolphin09@yahoo.co.id>
2016-05-27 13:48:13 +0700

    tambah controller kegiatan himpunan

--- application/controllers/kegiatan_himpunan.php
+++ application/controllers/kegiatan_himpunan.php
@@ -34,49 +34,49 @@ class Kegiatan_himpunan extends Private_Controller{
     $this->load_page('page/private/himpunan/list_kegiatan_himpunan', $data); 
     }
 
-    // function detail_kegiatan(){
-    //     $id_pengajuan = $this->input->get('id');
+    function detail_kegiatan(){
+        $id_pengajuan = $this->input->get('id');
 
-    //     $data['proposals'] = array();
-    //     foreach ($proposals as $proposal) {
+        $data['proposals'] = array();
+        foreach ($proposals as $proposal) {
 
-    //         array_push($data['proposals'], array(
-    //             'judul'             => $proposal->judul,
-    //             'tanggal_upload'    => $proposal->waktu_upload,
-    //             'status'            => $proposal->status_approve
-    //         ));
+            array_push($data['proposals'], array(
+                'judul'             => $proposal->judul,
+                'tanggal_upload'    => $proposal->waktu_upload,
+                'status'            => $proposal->status_approve
+            ));
             
-    //     }
-    //     // $results = array();
-    //     // foreach ($proposals as $proposal) {
-    //     //  $result = array(
-    //     //      'judul'             => $proposal->judul,
-    //     //      'tanggal_upload'    => $proposal->waktu_upload,
-    //     //      'status'            => $proposal->status_approve
-    //     //  );
-    //     //  array_push($results, $result);
-    //     // }
-    //     // json_encode($results);
-    //     $this->load_page('page/private/staff/logbook_proposal_himpunan_detail', $data);
+        }
+        // $results = array();
+        // foreach ($proposals as $proposal) {
+        //  $result = array(
+        //      'judul'             => $proposal->judul,
+        //      'tanggal_upload'    => $proposal->waktu_upload,
+        //      'status'            => $proposal->status_approve
+        //  );
+        //  array_push($results, $result);
+        // }
+        // json_encode($results);
+        $this->load_page('page/private/staff/logbook_proposal_himpunan_detail', $data);
 
-    //     if($id_pengajuan){
-    //         $proposals = $this->logbook_pengajuan_proposal_himpunan_model->get_many_by(array('pengaju' => $himpunan->id, 'status_approve' => 'y'));
-    //         $data['judul']              = $event->id;
-    //         $data['tanggal_upload']     = $event->nama_event;
-    //         $data['status']             = $event->status;
-    //         $data['penanggungjawab']    = $event->penanggungjawab;
+        if($id_pengajuan){
+            $proposals = $this->logbook_pengajuan_proposal_himpunan_model->get_many_by(array('pengaju' => $himpunan->id, 'status_approve' => 'y'));
+            $data['judul']              = $event->id;
+            $data['tanggal_upload']     = $event->nama_event;
+            $data['status']             = $event->status;
+            $data['penanggungjawab']    = $event->penanggungjawab;
 
-    //         $this->load_page('page/private/detail_event.php', $data);
-    //     } else {
-    //         $this->load->model('user_model');
-    //         $user = $this->user_model->get_by(array('id' => $this->session->userdata('id')));
+            $this->load_page('page/private/detail_event.php', $data);
+        } else {
+            $this->load->model('user_model');
+            $user = $this->user_model->get_by(array('id' => $this->session->userdata('id')));
 
-    //         if($user->role == 'mahasiswa'){
-    //             $data['events'] = $this->event_model->order_by('tanggal_event')->get_many_by(array('status' => 'disetujui'));
-    //         } else if($user->role == 'staff'){
-    //             $data['events'] = $this->event_model->order_by('tanggal_event')->get_all();
-    //         }
-    //         $this->load_page('page/private/event.php', $data);
-    //     }
-    // }
+            if($user->role == 'mahasiswa'){
+                $data['events'] = $this->event_model->order_by('tanggal_event')->get_many_by(array('status' => 'disetujui'));
+            } else if($user->role == 'staff'){
+                $data['events'] = $this->event_model->order_by('tanggal_event')->get_all();
+            }
+            $this->load_page('page/private/event.php', $data);
+        }
+    }
 }
