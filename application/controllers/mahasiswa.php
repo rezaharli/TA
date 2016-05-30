b804cd6ca7a1fa29b221414dc49ef0d2f1127b59
Antonilmiar <antonilmiar@gmail.com>
2016-05-30 15:08:44 +0700

    comit ben gak error

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -1,11 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Mahasiswa extends User {
+class Mahasiswa extends Private_Controller {
 
 	function __construct() {
 		parent::__construct();
-		$this->load->model('himpunan_model');
+		$this->load->model('user_model');
 	}
+
+    function do_reset_password($id){
+        $nim = $this->input->get('nim');
+
+        $this->user_model->update($id, array('password' => sha1($nim)));
+        redirect('mahasiswa/list');
+    }
 
 }
 
