5ee105d89410a44b78562223a2a52abfa3edb9cc
nurrida <nurrida.aini@gmail.com>
2016-06-01 22:41:17 +0700

    manage mahasiswa

--- application/views/page/private/staff/list_mahasiswa.php
+++ application/views/page/private/staff/list_mahasiswa.php
@@ -24,19 +24,18 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="box">
+                    
                     <div class="box-body">
                         <table id="tabel-mahasiswa" class="table table-bordered table-striped">
                             <thead>
                               <tr>
-                                <th>NIM</th>
-                                <th>Prodi</th>
-                                <th>Kelas</th>
-                                <th>Username</th>
-                                <th>Nama</th>
-                                <th>Email</th>
-                                <th>Alamat</th>
-                                <th>Telp</th>
-                                <th>Aksi</th>
+                                <th style="width: 40px;">NIM</th>
+                                <th style="width: 30px;">Prodi</th>
+                                <th style="width: 10px;">Kelas</th>
+                                <th style="width: 50px;">Username</th>
+                                <th >Nama</th>
+                                <th style="width: 200px;">Email</th>
+                                <th style="width: 190px; text-align: center;">Aksi</th>
                               </tr>
                             </thead>
                         </table>
--- assets/js/cek_username.js
+++ assets/js/cek_username.js
@@ -47,3 +47,103 @@ $("#input-username").on('focus change keyup', function(event){
           $("#form-group-username").addClass("has-error");
      }
 });
+
+$("#input-nip").on('focus change keyup', function(event){
+     var nip = $("#input-nip").val();
+
+     var segment = window.location.href.split("/");
+     
+     var url = window.location.origin + "/hmmmm/staff/do_nip_check";
+
+     // jika berada di halaman profil berarti aksi pasti edit selain itu bukan edit
+     if(segment[4] == 'staff') var aksi = 'tambah';
+
+     request = $.ajax({
+          url: url,
+          type: "post",
+          data: 'nip='+nip+'&aksi='+aksi+'&nip_lama='+segment[5]
+     });
+
+     if($("#input-nip").val() != ""){
+          request.done(function (response){
+               if(response != ''){
+                    if(response == '1'){ //jika username sudah digunakan
+                         $("#status-nip").html('Sudah digunakan');
+                         $("#form-group-nip").removeClass("has-success");
+                         $("#form-group-nip").addClass("has-error");
+                    } else { //jika username sama dengan sebelumnya
+                         $("#status-nip").html('');
+                         $("#form-group-nip").removeClass("has-success");
+                         $("#form-group-nip").removeClass("has-error");
+                    }
+                    $("#button-nip-submit").prop("disabled", true);
+                    $("#status-nip").removeClass("text-green");
+                    $("#status-nip").addClass("text-red");
+               } else if(response == ''){ // jika username belum digunakan
+                    $("#status-nip").html('Tersedia');
+                    $("#status-nip").removeClass("text-red");
+                    $("#status-nip").addClass("text-green");
+                    $("#button-nip-submit").prop("disabled", false);
+                    $("#form-group-nip").removeClass("has-error");
+                    $("#form-group-nip").addClass("has-success");
+               }
+         });
+     } else { // jika input tidak diisi
+          $("#status-nip").html('silahkan isi');
+          $("#status-nip").removeClass("text-green");
+          $("#status-nip").addClass("text-red");
+          $("#button-nip-submit").prop("disabled", true);
+          $("#form-group-nip").removeClass("has-success");
+          $("#form-group-nip").addClass("has-error");
+     }
+});
+
+$("#input-nim").on('focus change keyup', function(event){
+     var nim = $("#input-nim").val();
+
+     var segment = window.location.href.split("/");
+     
+     var url = window.location.origin + "/hmmmm/mahasiswa/do_nim_check";
+
+     // jika berada di halaman profil berarti aksi pasti edit selain itu bukan edit
+     if(segment[4] == 'mahasiswa') var aksi = 'tambah';
+
+     request = $.ajax({
+          url: url,
+          type: "post",
+          data: 'nim='+nim+'&aksi='+aksi+'&nim_lama='+segment[5]
+     });
+
+     if($("#input-nim").val() != ""){
+          request.done(function (response){
+               if(response != ''){
+                    if(response == '1'){ //jika username sudah digunakan
+                         $("#status-nim").html('Sudah digunakan');
+                         $("#form-group-nip").removeClass("has-success");
+                         $("#form-group-nip").addClass("has-error");
+                    } else { //jika username sama dengan sebelumnya
+                         $("#status-nim").html('');
+                         $("#form-group-nip").removeClass("has-success");
+                         $("#form-group-nip").removeClass("has-error");
+                    }
+                    $("#button-nim-submit").prop("disabled", true);
+                    $("#status-nim").removeClass("text-green");
+                    $("#status-nim").addClass("text-red");
+               } else if(response == ''){ // jika username belum digunakan
+                    $("#status-nim").html('Tersedia');
+                    $("#status-nim").removeClass("text-red");
+                    $("#status-nim").addClass("text-green");
+                    $("#button-nim-submit").prop("disabled", false);
+                    $("#form-group-nip").removeClass("has-error");
+                    $("#form-group-nip").addClass("has-success");
+               }
+         });
+     } else { // jika input tidak diisi
+          $("#status-nim").html('silahkan isi');
+          $("#status-nim").removeClass("text-green");
+          $("#status-nim").addClass("text-red");
+          $("#button-nim-submit").prop("disabled", true);
+          $("#form-group-nim").removeClass("has-success");
+          $("#form-group-nim").addClass("has-error");
+     }
+});
