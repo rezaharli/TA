d19b101cffea29f2e2a1d19f06ef54cae5f0b875
Antonilmiar <antonilmiar@gmail.com>
2016-06-24 04:02:08 +0700

    komit logbook seng iso plus

--- application/views/page/private/mahasiswa/logbook_detail_proposal.php
+++ application/views/page/private/mahasiswa/logbook_detail_proposal.php
@@ -22,91 +22,120 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <section class="content">
         <div class="row">
         <!-- left column -->
-            <div class="col-md-6">
-                <div class="box box-primary">
-                    <div class="box-body">
-                        <div class="table-responsive">
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Penyelenggara
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $penyelenggara ?></pre>
-                            </div>
-
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Tingkat Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $tingkat_kompetisi ?></pre>
-                            </div>
-
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Kategori Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $kategori_kompetisi ?></pre>
-                            </div>
-
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Tujuan Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $tujuan_kompetisi ?></pre>
-                            </div>
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Sasaran Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $sasaran_kompetisi ?></pre>
-                            </div>
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Tanggal Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $tanggal_kompetisi  ?></pre>
-                            </div>
-                        </div>
+            <div class="md-col-12">
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Penyelenggara
+                        </h4>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="box box-primary">
                     <div class="box-body">
-                        <div class="table-responsive">
-                            
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Tempat Kompetisi
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $tempat_kompetisi ?></pre>
-                            </div>
-
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Anggaran
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $anggaran_biaya ?></pre>
-                            </div>
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Nama Tim
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $nama_tim ?></pre>
-                            </div>
-                            <div class="col-sm-12 box-header with-border">
-                                <class="box-title"># Pembimbing
-                            </div><!-- /.box-header -->
-                            <div class="col-sm-12">
-                                <pre class="prettyprint"><?php echo $pembimbing ?></pre>
-                            </div>
-                            <div class="col-sm-12">
-                                <a href="<?php echo base_url('proposal/detail_tim?id_proposal='.$proposal); ?>">
-                                    <button class="btn btn-sm btn-info"><i class="fa fa-list"></i>&nbsp;Detail Tim</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                        <pre class="prettyprint"><?php echo $penyelenggara ?></pre>
+                  </div><!-- /.box-body -->
                 </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Tingkat Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $tingkat_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Kategori Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $kategori_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Tujuan Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $tujuan_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Sasaran Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $sasaran_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Tanggal Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $tanggal_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Tempat Kompetisi
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $tempat_kompetisi ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Anggaran
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $anggaran_biaya ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Nama Tim
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $nama_tim ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+                <div class="box box-solid collapsed-box">
+                    <div class="box-header bg-light-blue-gradient">
+                        <h4 class="box-title">
+                          <button class="btn btn-primary btn-xs" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                              &nbsp;Pembimbing
+                        </h4>
+                    </div>
+                    <div class="box-body">
+                        <pre class="prettyprint"><?php echo $pembimbing ?></pre>
+                  </div><!-- /.box-body -->
+                </div>
+
+
+
             </div>
         <!-- right column -->
             
