c2a4180421099f60e8a12cebf14b35216a96c690
Reza Harli <reza.harli@gmail.com>
2016-06-27 12:49:36 +0700

    change rekomendasi to cetak via email

--- application/views/email/rekomendasi_beasiswa.php
+++ application/views/email/rekomendasi_beasiswa.php
@@ -1,6 +1,6 @@
 <html>
 	<body>
 		<h1>Selamat, <?php echo $nama ?>. Anda berhak untuk melakukan proses pengajuan beasiswa.</h1>
-		<p>Silahkan ajukan beasiswa anda ke Bagian Kemahasiswaan</p>
+		<p>Silahkan klik link <a href="<?php echo $link ?>">ini</a> untuk mencetak surat pengajuan beasiswa anda.</p>
 	</body>
 </html>
